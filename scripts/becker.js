54e1e71b75ab4893dd2809a351392a1a16b6ab4b
André Morujão <amorujao@gmail.com>
2015-06-02 10:21:32 +0100

    Minor tweak to kung fury quote

--- scripts/becker.js
+++ scripts/becker.js
@@ -58,7 +58,7 @@ module.exports = function(robot) {
   ];
 
   var kungfury = [
-    "Kung Fury: _You don't need that spine._ \n[Grabs Nazi soldier by the head and rips out his spine]\n _It's holding you back._",
+    "Kung Fury: _You don't need that spine._ \n[Grabs Nazi soldier by the head and rips out his spine]\nKung Fury: _It's holding you back._",
     "Kung Fury: _I'm disarming you._ [rips Nazi soldier's arm off]",
     "Hacker Man: _Wait a minute. Using an RX modulator, I might be able to conduct a mainframe cell layer and hack you into the download._ \nKung Fury: _What the hell does that mean?_ \nHacker Man: _It means that with the right computer algorithms, I can hack you back in time. Just like a time machine._",
     "Kung Fury: _Fuck! That's a laser raptor. I thought they went extinct thousands of years ago._",
