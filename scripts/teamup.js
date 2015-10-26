cccabf3e1990e85946066a8fcb88375c64dee768
André Morujão <amorujao@gmail.com>
2015-10-26 14:01:12 +0000

    Add images for team matchups

--- scripts/teamup.js
+++ scripts/teamup.js
@@ -16,28 +16,29 @@
 module.exports = function(robot) {
 
     var teamPairs = [
+
 	// animals
-	["Hawks", "Eagles"],
-	["Velociraptors", "Tyrannosaurus Rex"],
+	["Hawks", "Eagles", "https://f1.bcbits.com/img/a3151354777_10.jpg"],
+	["Velociraptors", "Tyrannosaurus Rex", "http://img15.deviantart.net/9d52/i/2011/133/0/3/jp_t_rex_vs__velociraptors_by_yankeetrex-d3g87qw.jpg"],
 	["Cobras", "Vipers"],
-	["Bulls", "Bears"],
+	["Bulls", "Bears", "http://wwwcdn3.wyattresearch.com/wp-content/uploads/2014/07/470467481.jpg"],
 	["Wolves", "Coyotes"],
-	["Sharks", "Killer Whales"],
-	["Cobras", "Pythons"],
-	["Leopards", "Cheetahs"],
+	["Sharks", "Killer Whales", "http://www.lifeintheknow.com/wp-content/uploads/2014/07/killer-whale-kills-great-white.jpg"],
+	["Cobras", "Pythons", "http://37.media.tumblr.com/tumblr_ma9q1yebe21ro8kzyo1_1280.jpg"],
+	["Leopards", "Cheetahs", "https://s3.amazonaws.com/assets.whatiworeblog.com/2013/November/Nov+09+%7C+Meow/Cheetah+vs+Leopard+Print.jpg"],
 	["Scorpions", "Snakes"],
 
 	// movies & animated series
-	["Crouching Tigers", "Hidden Dragons"]
-	["Cobra Kai Dojo", "Miyagi Dojo"],
-	["Jedi", "Sith"],
-	["Decepticons", "Transformers"],
+	["Crouching Tigers", "Hidden Dragons"],
+	["Cobra Kai Dojo", "Miyagi Dojo", "http://3.bp.blogspot.com/-MbRNpe9HnYQ/TnJpqaJBszI/AAAAAAAAAao/GQKLXtzS7pY/s1600/dojo2.jpg"],
+	["Jedi", "Sith", "http://images-cdn.moviepilot.com/image/upload/c_fill,h_212,w_500/t_mp_quality/perso_skywalker_luke_17-jedi-vs-sith-who-are-the-real-good-guys-star-wars-episode-7-hopes-jpeg-170604.jpg"],
+	["Decepticons", "Autobots", "http://openwalls.com/image/15508/thumb3_transformers_autobot_vs_decepticons_1.jpg"],
 	["X-Men", "Watchmen"],
 	["A-Team", "Expendables"],
 	["Reservoir Dogs", "Inglourious Basterds"],
 
 	// sci-fi channel movies
-	["Mega Python", "Gatoroid"],
+	["Mega Python", "Gatoroid", "http://ia.media-imdb.com/images/M/MV5BMjEwODY1ODQ3Nl5BMl5BanBnXkFtZTgwMTAyMjM5MTE@._V1_SX640_SY720_.jpg"],
 	["Dinocroc", "Supergator"],
 	["Komodo", "Cobra"],//technically these would also fit the animals category
 	["Piranhaconda", "Frankenfish"],
@@ -74,6 +75,9 @@ module.exports = function(robot) {
 
         var teamNames = teamPairs[Math.floor(Math.random() * teamPairs.length)];
 
+        if (teamNames.length == 3) {
+            msg.send(teamNames[2]);
+        }
         msg.send(teamNames[0] + ": " + team1.join(", ") + "\n" + teamNames[1] + ": " + team2.join(", "));
     });
 };
