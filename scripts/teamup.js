40aaf094473792da58ceaa1160f4c9b9075d9d45
André Morujão <amorujao@gmail.com>
2016-11-08 09:48:20 +0000

    Add futsal score for 2016-11-07

--- scripts/teamup.js
+++ scripts/teamup.js
@@ -46,7 +46,8 @@ module.exports = function(robot) {
 		{date:"2016-10-10", players:[["am", "jd", "pv", "Tiago Ferreira"],["jr", "ns", "rp", "jc"]], score:[6, 6]},
 		{date:"2016-10-17", players:[["am", "jd", "rf", "rp", "sa"],["jr", "ns", "jm", "jc", "pv"]], score:[11, 4]},
 		{date:"2016-10-24", players:[["am", "jr", "rf", "jm", "ns"],["rp", "rg", "sa", "jc", "pv"]], score:[8, 10]},
-		{date:"2016-10-31", players:[["am", "pv", "jc", "ns"],["rp", "rf", "sa", "Alcobaça"]], score:[8, 7]}
+		{date:"2016-10-31", players:[["am", "pv", "jc", "ns"],["rp", "rf", "sa", "Alcobaça"]], score:[8, 7]},
+		{date:"2016-11-07", players:[["pv", "jr", "ns", "jm"],["rp", "rf", "sa", "jc"]], score:[6, 8]}
 	];
 
 	// each item: [<aliases>, <nicknames>, <short name>]
